d8ed2844ba8c06b1ff4964ba96c1ffe6f22f6b29
Saniah <sgthompson@ualr.edu>
2023-02-09 20:01:03 +0000

    trying to get operator in calculator to work

--- calculator/assets/script/minmaxavg.js
+++ calculator/assets/script/minmaxavg.js
@@ -5,57 +5,53 @@ function calculate() {
         
         /* get the operands from the form */
         var operand1 = document.getElementById("Operand1").value;
+        var operator = document.getElementById("Operator").value;
         var operand2 = document.getElementById("Operand2").value;
-        var operand3 = document.getElementById("Operand3").value;
         
         /* convert the operands from string to floating point */
         var operand1fp = parseFloat (operand1);
+        var operatorfp = parseFloat (operator);
         var operand2fp = parseFloat (operand2);
-        var operand3fp = parseFloat (operand3);
         
         /* figure out which operator was checked and place the value in operator */
         var operator;
-        if (document.getElementById("MinOperator").checked) {
-            operator = document.getElementById("MinOperator").value;
+        if (document.getElementById("PlusOperator").checked) {
+            operator = document.getElementById("PlusOperator").value;
         }
-        if (document.getElementById("MaxOperator").checked) {
-            operator = document.getElementById("MaxOperator").value;
+        if (document.getElementById("MinusOperator").checked) {
+            operator = document.getElementById("MinusOperator").value;
         }
-        if (document.getElementById("AvgOperator").checked) {
-            operator = document.getElementById("AvgOperator").value;
+        if (document.getElementById("TimesOperator").checked) {
+            operator = document.getElementById("TimesOperator").value;
+        }
+        if (document.getElementById("DivideOperator").checked) {
+            operator = document.getElementById("DivideOperator").value;
         }
 
         var result;
         
-        /* if the operator was "Min" then set result to the minimum */
-        if (operator == "Min") {
-            if(operand1fp <= operand2fp && operand1fp <= operand3fp) {
-                result = operand1fp;
-            }
-            if(operand2fp <= operand1fp && operand2fp <= operand3fp) {
-                result = operand2fp;
-            }
-            if(operand3fp <= operand1fp && operand3fp <= operand2fp) {
-                result = operand3fp;
+        /* if the operator was "Plus" then add for result */
+        if (operator == "Plus") {
+            if(operand1fp + operand2fp == result);{
             }
         }
  
-        /* if the operator was "Max" then set result to the maximum */
-        if (operator == "Max") {
-            if(operand1fp >= operand2fp && operand1fp >= operand3fp) {
-                result = operand1fp;
-            }
-            if(operand2fp >= operand1fp && operand2fp >= operand3fp) {
-                result = operand2fp;
-            }
-            if(operand3fp >= operand1fp && operand3fp >= operand2fp) {
-                result = operand3fp;
+        /* if the operator was "Minus" then subtract for result */
+        if (operator == "Minus") {
+            if(operand1fp - operand2fp == result);{
             }
         }
 
-        /* if operator was "Avg" the calcualute the average of 3 operands */
-        if (operator == "Avg") {
-            result = (operand1fp + operand2fp + operand3fp) / 3.0;
+        /* if operator was "Times" then multiply for result */
+        if (operator == "Times") {
+            if(operand1fp * operand2fp == result);{
+            }
+        }
+
+        /* if operator was "Divide" then divide for result */
+        if (operator == "Divide") {
+            if(operand1fp / operand2fp == result);{
+            }
         }
         
         /* convert the result to a string and display it */
@@ -67,15 +63,15 @@ function clearform() {
     
     /* Set all of the form values to blank or false */
     document.getElementById("Operand1").value = "";
+    document.getElementById("Operator").value = "";
     document.getElementById("Operand2").value = "";
-    document.getElementById("Operand3").value = "";
     document.getElementById("Operand1Error").innerHTML = "";
-    document.getElementById("Operand2Error").innerHTML = "";
-    document.getElementById("Operand3Error").innerHTML = "";
-    document.getElementById("MinOperator").checked = false;
-    document.getElementById("MaxOperator").checked = false;
-    document.getElementById("AvgOperator").checked = false;
     document.getElementById("OperatorError").innerHTML = "";
+    document.getElementById("Operand2Error").innerHTML = "";
+    document.getElementById("PlusOperator").checked = false;
+    document.getElementById("MinusOperator").checked = false;
+    document.getElementById("TimesOperator").checked = false;
+    document.getElementById("DivideError").innerHTML = "";
     document.getElementById("Result").innerHTML = "";
 }
 
