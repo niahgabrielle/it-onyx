0927442ecffe52fd175f3cbbe46df5429a6e43c1
Saniah <sgthompson@ualr.edu>
2023-04-20 21:09:52 +0000

    merging code

--- buffalo-river/assets/js/river.js
+++ buffalo-river/assets/js/river.js
@@ -1,43 +1,219 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+async function GetData() {
+  var myURL = "https://waterservices.usgs.gov/nwis/iv/?format=json&indent=on&sites=07055646,07055660,07055680,07055780&period=P7D&parameterCd=00065&siteStatus=active";
+  var msgObject = await fetch(myURL);
+  var msgJSONText = await msgObject.text();
+  var msg = JSON.parse(msgJSONText);  
+          /* Site 1 */
+          var dates = [];
+          var values = [];
+          /* fLen contains the length of the array (number of values) */
+          fLen = msg.value.timeSeries[0].values[0].value.length
+          for (i = 0; i < fLen; i++) {
+              values[i] = msg.value.timeSeries[0].values[0].value[i].value
+              dates[i] = msg.value.timeSeries[0].values[0].value[i].dateTime
+          }
+        
+          var sitename = msg.value.timeSeries[0].sourceInfo.siteName
+          var sitecode = msg.value.timeSeries[0].sourceInfo.siteCode[0].value
+          var siteDescription = msg.value.timeSeries[0].variable.variableDescription
+      
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+var ctx = document.getElementById("chartjs-0");
+var myChart = new Chart(ctx, {
+    "type":"line",
+    "data": {
+    "labels": dates,
+    "datasets":[{"label":"Gauge Height",
+    "data": values,
+    "fill":false,
+    "borderColor":"#65333A",
+    "lineTension":0.1}]},
+    "options":{
+          responsive: false,
+          maintainAspectRatio: true,
+          title: {
+              display: true,
+              text: sitename,
+              fontSize : 18
+     },
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+          scales: {
+          xAxes: [{
+          type: 'time',
+          time: {
+          displayFormats: {
+          day: 'MMM D'
+         }
+     }
+               }],
+          
+          yAxes: [{
+          scaleLabel: {
+          display: true,
+          labelString: siteDescription
+        }
+         }]
+          
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+      }
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+      );
+          
+var dates = [];
+var values = [];
+/* fLen contains the length of the array (number of values) */
+fLen = msg.value.timeSeries[1].values[0].value.length
+for (i = 0; i < fLen; i++) {
+values[i] = msg.value.timeSeries[1].values[0].value[i].value
+dates[i] = msg.value.timeSeries[1].values[0].value[i].dateTime
 }
+var sitename = msg.value.timeSeries[1].sourceInfo.siteName
+var sitecode = msg.value.timeSeries[1].sourceInfo.siteCode[0].value
+var siteDescription = msg.value.timeSeries[1].variable.variableDescription
 
-var acc = document.getElementsByClassName("accordion");
-var i;
+var ctx0 = document.getElementById("chartjs-1");
+var myChart = new Chart(ctx0, {
+"type":"line",
+"data": {
+"labels": dates, 
+"datasets":[{"label":"Gauge Height",
+"data": values,
+"fill":false,
+"borderColor":"#65333A",
+"lineTension":0.1}]},
+"options":{ 
+  responsive: false,
+  maintainAspectRatio: true,
+  title: {
+      display: true,
+      text: sitename,
+      fontSize : 18
+  },
+  scales: {
+      xAxes: [{
+          type: 'time',
+          time: {
+               displayFormats: {
+                  day: 'MMM D'
+              }
+          }
+      }],
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
+      yAxes: [{
+          scaleLabel: {
+              display: true,
+              labelString: siteDescription
+          }
+      }]
+
+  }
+}
+});
+
+var dates = [];
+var values = [];
+/* fLen contains the length of the array (number of values) */
+fLen = msg.value.timeSeries[2].values[0].value.length
+for (i = 0; i < fLen; i++) {
+values[i] = msg.value.timeSeries[2].values[0].value[i].value
+dates[i] = msg.value.timeSeries[2].values[0].value[i].dateTime
+}
+var sitename = msg.value.timeSeries[2].sourceInfo.siteName
+var sitecode = msg.value.timeSeries[2].sourceInfo.siteCode[0].value
+var siteDescription = msg.value.timeSeries[2].variable.variableDescription
+
+var ctx0 = document.getElementById("chartjs-2");
+var myChart = new Chart(ctx0, {
+"type":"line",
+"data": {
+"labels": dates, 
+"datasets":[{"label":"Gauge Height",
+"data": values,
+"fill":false,
+"borderColor":"#65333A",
+"lineTension":0.1}]},
+"options":{ 
+  responsive: false,
+  maintainAspectRatio: true,
+  title: {
+      display: true,
+      text: sitename,
+      fontSize : 18
+  },
+  scales: {
+      xAxes: [{
+          type: 'time',
+          time: {
+               displayFormats: {
+                  day: 'MMM D'
+              }
+          }
+      }],
+
+      yAxes: [{
+          scaleLabel: {
+              display: true,
+              labelString: siteDescription
+          }
+      }]
+
+  }
+  
+} }
+);
+
+
+var dates = [];
+var values = [];
+/* fLen contains the length of the array (number of values) */
+fLen = msg.value.timeSeries[3].values[0].value.length
+for (i = 0; i < fLen; i++) {
+values[i] = msg.value.timeSeries[3].values[0].value[i].value
+dates[i] = msg.value.timeSeries[3].values[0].value[i].dateTime
+}
+var sitename = msg.value.timeSeries[3].sourceInfo.siteName
+var sitecode = msg.value.timeSeries[3].sourceInfo.siteCode[0].value
+var siteDescription = msg.value.timeSeries[3].variable.variableDescription
+
+var ctx0 = document.getElementById("chartjs-3");
+var myChart = new Chart(ctx0, {
+"type":"line",
+"data": {
+"labels": dates, 
+"datasets":[{"label":"Gauge Height",
+"data": values,
+"fill":false,
+"borderColor":"#65333A",
+"lineTension":0.1}]},
+"options":{ 
+  responsive: false,
+  maintainAspectRatio: true,
+  title: {
+      display: true,
+      text: sitename,
+      fontSize : 18
+  },
+
+  scales: {
+      xAxes: [{
+          type: 'time',
+          time: {
+               displayFormats: {
+                  day: 'MMM D'
+              }
+          }
+      }],
+
+      yAxes: [{
+          scaleLabel: {
+              display: true,
+              labelString: siteDescription
+          }
+      }]
+
+  }
+}
+});
+
 }
